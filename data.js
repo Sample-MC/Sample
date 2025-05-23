e70c096cfa6f135c6b1dac8ebcbac2c3352718f3
Sample-MC <medicalcard73@gmail.com>
2025-05-24 00:11:50 +0530

    data.js

--- data.js
+++ data.js
@@ -1,32 +1,34 @@
-const employees = {
-  "EMP001": {
-    "name": "Sushil Kumar",
-    "designation": "Deputy Safety Manager",
-    "age": "33",
-    "dob": "1991-11-14 00:00:00",
-    "gender": "Male",
-    "contact": "7339395251",
-    "emergency_contact": "6374276596",
-    "relationship": "Wife",
-    "company_address": " Plot No-52, Kanchipuram, 1st Cross St, Mahindra World City, 8th Avenue, Anjur R.F., Tamil Nadu 603002",
-    "residential_address": "49G/3A, Pallikoodathan Street, Kanchipuram - 631501",
-    "blood_group": "o+ve",
-    "insurance_no": "IL22411950300",
-    "photo": NaN
+const employees = [
+  {
+    id: "EMP001",
+    name: "Sushil Kumar",
+    age: 33,
+    designation: "Deputy Safety Manager",
+    dob: "14-11-1991",
+    gender: "Male",
+    contact: "7339395251",
+    emergency_contact: "6374276596",
+    relationship: "Wife",
+    company_address: "Plot No-52, Kanchipuram, 1st Cross St, Mahindra World City, 8th Avenue, Anjur R.F., Tamil Nadu- 603002",
+    residential_address: "49G/3A, Pallikoodathan Street, Kanchipuram - 631501",
+    blood_group: "O+ve",
+    medical_insurance: "IL22411950300",
+    photo: "EMP001.jpg"
   },
-  "EMP002": {
-    "name": "Dinesh Navin",
-    "designation": "Safety Officer",
-    "age": "26",
-    "dob": "02.08.1998",
-    "gender": "Male",
-    "contact": "7397417027",
-    "emergency_contact": "9003285249",
-    "relationship": "Wife",
-    "company_address": " Plot No-52, Kanchipuram, 1st Cross St, Mahindra World City, 8th Avenue, Anjur R.F., Tamil Nadu 603002",
-    "residential_address": " 14, 2nd Street Rajiv Gandhi Nagar (Near Rail Nagar), Kattankulathur, 603203, Chengalpattu, TN. ",
-    "blood_group": "B-ve",
-    "insurance_no": "IL22875198600",
-    "photo": NaN
+  {
+    id: "EMP002",
+    name: "Ravi Sharma",
+    age: 29,
+    designation: "Field Technician",
+    dob: "22-05-1995",
+    gender: "Male",
+    contact: "9876543210",
+    emergency_contact: "9123456789",
+    relationship: "Father",
+    company_address: "Tower 5, Chennai Industrial Zone, Tamil Nadu - 600100",
+    residential_address: "22B, Thiruvalluvar Street, Chennai - 600041",
+    blood_group: "B+ve",
+    medical_insurance: "AB123456789",
+    photo: "EMP002.jpg"
   }
-};
+];
