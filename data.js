666a1a63c489f84b72b3d0e0df60125e18b40e78
Sample-MC <medicalcard73@gmail.com>
2025-05-23 23:11:56 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const employees = {
+  "EMP001": {
+    "name": "Sushil Kumar",
+    "designation": "Deputy Safety Manager",
+    "age": "33",
+    "dob": "1991-11-14 00:00:00",
+    "gender": "Male",
+    "contact": "7339395251",
+    "emergency_contact": "6374276596",
+    "relationship": "Wife",
+    "company_address": " Plot No-52, Kanchipuram, 1st Cross St, Mahindra World City, 8th Avenue, Anjur R.F., Tamil Nadu 603002",
+    "residential_address": "49G/3A, Pallikoodathan Street, Kanchipuram - 631501",
+    "blood_group": "o+ve",
+    "insurance_no": "IL22411950300",
+    "photo": NaN
+  },
+  "EMP002": {
+    "name": "Dinesh Navin",
+    "designation": "Safety Officer",
+    "age": "26",
+    "dob": "02.08.1998",
+    "gender": "Male",
+    "contact": "7397417027",
+    "emergency_contact": "9003285249",
+    "relationship": "Wife",
+    "company_address": " Plot No-52, Kanchipuram, 1st Cross St, Mahindra World City, 8th Avenue, Anjur R.F., Tamil Nadu 603002",
+    "residential_address": " 14, 2nd Street Rajiv Gandhi Nagar (Near Rail Nagar), Kattankulathur, 603203, Chengalpattu, TN. ",
+    "blood_group": "B-ve",
+    "insurance_no": "IL22875198600",
+    "photo": NaN
+  }
+};
