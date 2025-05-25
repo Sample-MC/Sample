705696b145f111db9947bdb750f41120d9616313
Sample-MC <medicalcard73@gmail.com>
2025-05-26 00:23:57 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,33 @@
-const employeeData = [
-  {
-    "EMP ID": "101",
-    "Name": "Alice Johnson",
-    "Phone": "123-456-7890",
-    "Emergency Contact": "Bob Johnson - 987-654-3210"
+const employeeData = {
+  "emp001": {
+    "name": "Sushil Kumar",
+    "age": 33,
+    "designation": "Deputy Safety Manager",
+    "dob": "14-11-1991",
+    "gender": "Male",
+    "contact": "7339395251",
+    "emergency_contact": "7339395251",
+    "relationship": "Wife",
+    "company_address": " Plot No-52, Kanchipuram, 1st Cross St, Mahindra World City, 8th Avenue, Anjur R.F., Tamil Nadu 603002",
+    "residential_address": "49G/3A, Pallikoodathan Street, Kanchipuram - 631501",
+    "blood_group": "o+ve",
+    "insurance": "IL22411950300",
+    "photo": "https://sample-mc.github.io/Sample/photos/emp_001.png"
   },
-  {
-    "EMP ID": "102",
-    "Name": "John Smith",
-    "Phone": "555-555-5555",
-    "Emergency Contact": "Jane Smith - 444-444-4444"
+  "emp002": {
+    "name": "Dinesh Navin",
+    "age": 26,
+    "designation": "Safety Officer",
+    "dob": "02.08.1998",
+    "gender": "Male",
+    "contact": "7397417027",
+    "emergency_contact": "9003285249",
+    "relationship": "Wife",
+    "company_address": "Plot No-52, Kanchipuram, 1st Cross St, Mahindra World City, 8th Avenue, Anjur R.F., Tamil Nadu 603002",
+    "residential_address": " 14, 2nd Street Rajiv Gandhi Nagar (Near Rail Nagar), Kattankulathur, 603203, Chengalpattu, TN. ",
+    "blood_group": "B-ve",
+    "insurance": "IL22875198600",
+    "photo": "https://sample-mc.github.io/Sample/photos/emp_002.png"
   }
-];
+  // Add more employees...
+};
